532d9000725f4bda34790f74e84a23a7fe632f70
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-09-11 01:25:52 -0500

    refactor: deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/se_project_react/",
   plugins: [react()],
   server: {
     port: 3000,
