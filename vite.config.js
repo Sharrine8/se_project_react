5c8ab0e14be58d92fe0be23d4329bcd3539e9ac8
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2025-02-10 16:32:06 -0600

    fix: deployment + itemCard image object-fit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/se_project_react", //add for github pages deployment
   plugins: [react()],
   server: {
     port: 3000,
